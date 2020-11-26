c05184bd2a1162318fec13c05e69be17cab793a6
Melidorado <meliidorado@hotmail.com>
2020-11-26 18:29:58 -0300

    maquetado basico sin modales

---
+++ index.js
@@ -0,0 +1,4 @@
+const grilla = document.querySelector(".grilla")
+/* const botonNuevoJuego = document.querySelector("#nuevo-juego") */
+const botonReiniciarJuego = document.querySelector("#boton-reiniciar-juego")
+ 
