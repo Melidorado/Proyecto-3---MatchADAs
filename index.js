c46b9fc79edf316db60171438220f8269ac27fe9
sotodeborahcar <soto.dc.es@hotmail.com>
2020-11-30 18:47:19 -0300

    agrego funcion del reloj

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const grilla = document.querySelector(".grilla")
+const grilla = document.querySelector(".grilla");
 /* const botonNuevoJuego = document.querySelector("#nuevo-juego") */
-const botonReiniciarJuego = document.querySelector("#boton-reiniciar-juego")
- 
+const botonReiniciarJuego = document.querySelector("#boton-reiniciar-juego");
+
+let actualizarReloj = () => {
+  let segundosTimer = 30;
+
+  let timer = () => {
+    let segundosTotales = segundosTimer;
+
+    minutos = Math.floor(segundosTotales / 60);
+    minutos = (minutos < 10 ? "0" : "") + minutos;
+
+    segundosTotales %= 60;
+    segundosTotales = (segundosTotales < 10 ? "0" : "") + segundosTotales;
+
+    document.getElementById("temporizador").innerHTML =
+      minutos + ":" + segundosTotales;
+
+    segundosTimer--;
+
+    if (segundosTimer > -1) {
+      setTimeout(timer, 1000);
+    }
+  };
+  timer();
+};
+actualizarReloj("fin");
